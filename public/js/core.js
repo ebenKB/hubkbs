ec22526245f8efc8764640c8d691f3033cb0921e
ebenKB <eakbo23@gmail.com>
2019-03-01 09:31:39 +0000

    generate site

---
+++ public/js/core.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  // alert('the window has loaded...')
+  // fetch('https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp=1331161200&key=YOUR_API_KEY')
+  // .then((data) => {
+  //   console.log('this is the data from fetch :', data)
+  // })
+}
