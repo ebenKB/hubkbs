411a1fe2e45757fdcdf30094ddd96a2f64370192
Ebenezer K.B Adjei <eakbo23@gmail.com>
2022-12-31 05:30:36 +0000

    new build

--- public/js/popup.js
+++
@@ -1,9 +0,0 @@
-$("document").ready(() => {
-    setTimeout(() => {
-      $('#add-popup').removeClass('hide');
-    }, 2500);
-  });
-
-  $("#promo-close").on('click', (() => {
-    $('#add-popup').addClass('hide');
-  }));
